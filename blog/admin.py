6696fced75a90b010372b841ad41009ad466ef3e
Davermad <danielwebd17@gmail.com>
2025-01-21 20:29:16 +0600

    Create API Swagger Documentation

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Publication
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'icon')
     search_fields = ('name',)
+
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'category', 'content', 'is_archived', 'created', 'updated')
+    list_filter = ('is_archived', 'category', 'created', 'updated')
+    search_fields = ('content', 'user__username', 'category__name')
+    ordering = ('-created',)
+    readonly_fields = ('created', 'updated')
+
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'category', 'content', 'image', 'is_archived')
+        }),
+        ('Техническая информация', {
+            'fields': ('created', 'updated'),
+        }),
+    )
